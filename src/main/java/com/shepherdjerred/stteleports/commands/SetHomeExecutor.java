ef05fa0d6ef8209729c5691f5a1c7941005135d7
Jerred Shepherd <ShepherdJerred@gmail.com>
2016-08-02 14:58:49 -0500

    Add sethome command

--- src/main/java/com/shepherdjerred/stteleports/commands/SetHomeExecutor.java
+++ src/main/java/com/shepherdjerred/stteleports/commands/SetHomeExecutor.java
@@ -1,4 +1,33 @@
 package com.shepherdjerred.stteleports.commands;
 
-public class SetHomeExecutor {
+import com.shepherdjerred.stteleports.objects.TeleportPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetHomeExecutor implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Players only");
+            return true;
+        }
+
+        if (!sender.hasPermission("stTeleports.sethome")) {
+            sender.sendMessage("No permission");
+            return true;
+        }
+
+        TeleportPlayer teleportPlayer = TeleportPlayer.getTeleportPlayer(((Player) sender).getUniqueId());
+
+        teleportPlayer.setHome(((Player) sender).getLocation());
+        sender.sendMessage("Set home");
+
+        return true;
+
+    }
+
+
 }
