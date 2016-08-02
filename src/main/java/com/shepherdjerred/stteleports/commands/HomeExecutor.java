2f0da245ebec1361de200b2c640c39f652ef3dd8
Jerred Shepherd <ShepherdJerred@gmail.com>
2016-08-02 14:58:36 -0500

    Add home command

--- src/main/java/com/shepherdjerred/stteleports/commands/HomeExecutor.java
+++ src/main/java/com/shepherdjerred/stteleports/commands/HomeExecutor.java
@@ -1,4 +1,32 @@
 package com.shepherdjerred.stteleports.commands;
 
-public class HomeExecutor {
+import com.shepherdjerred.stteleports.objects.TeleportPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HomeExecutor implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Players only");
+            return true;
+        }
+
+        if (!sender.hasPermission("stTeleports.home")) {
+            sender.sendMessage("No permission");
+            return true;
+        }
+
+        TeleportPlayer teleportPlayer = TeleportPlayer.getTeleportPlayer(((Player) sender).getUniqueId());
+
+        ((Player) sender).teleport(teleportPlayer.getHome());
+        sender.sendMessage("Teleported home");
+
+        return true;
+
+    }
+
 }
