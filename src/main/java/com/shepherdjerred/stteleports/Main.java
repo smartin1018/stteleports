408106df3e91692f8b874e40f8fc6841415e15f4
Jerred Shepherd <ShepherdJerred@gmail.com>
2016-08-08 10:15:18 -0500

    Add periodic decrement, handle reloads

--- src/main/java/com/shepherdjerred/stteleports/Main.java
+++ src/main/java/com/shepherdjerred/stteleports/Main.java
@@ -2,16 +2,19 @@ package com.shepherdjerred.stteleports;
 
 import com.shepherdjerred.stteleports.commands.*;
 import com.shepherdjerred.stteleports.files.ConfigHelper;
+import com.shepherdjerred.stteleports.files.FileManager;
 import com.shepherdjerred.stteleports.listeners.JoinListener;
 import com.shepherdjerred.stteleports.listeners.QuitListener;
 import com.shepherdjerred.stteleports.metrics.MetricsLite;
+import com.shepherdjerred.stteleports.objects.TeleportPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class Main extends JavaPlugin {
-
-    public static boolean debug;
 
     private static Main instance;
 
@@ -28,6 +31,8 @@ public class Main extends JavaPlugin {
 
         ConfigHelper.loadConfigs();
 
+        runDecrement();
+
         this.getCommand("stteleports").setExecutor(new MainExecutor());
 
         this.getCommand("teleport").setExecutor(new TPExecutor());
@@ -42,9 +47,61 @@ public class Main extends JavaPlugin {
             MetricsLite metrics = new MetricsLite(this);
             metrics.start();
         } catch (IOException e) {
-
+            getLogger().info("Error enabling metrics");
+            e.printStackTrace();
         }
+
+        Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+            TeleportPlayer teleportPlayer = new TeleportPlayer(player.getUniqueId());
+
+            if (FileManager.getInstance().storage.getConfigurationSection("players." + player.getUniqueId() + ".cooldown") != null)
+                teleportPlayer.setCooldown(FileManager.getInstance().storage.getLong("players." + player.getUniqueId() + ".cooldown"));
+
+            if (FileManager.getInstance().storage.getConfigurationSection("players." + player.getUniqueId() + ".cooldown-multiplier") != null)
+                teleportPlayer.setCooldownMultiplier(FileManager.getInstance().storage.getDouble("players." + player.getUniqueId() + ".cooldown-multiplier"));
+
+            if (FileManager.getInstance().storage.getConfigurationSection("players." + player.getUniqueId() + ".cost-multiplier") != null)
+                teleportPlayer.setCostMultiplier(FileManager.getInstance().storage.getDouble("players." + player.getUniqueId() + ".cost-multiplier"));
+
+            if (FileManager.getInstance().storage.getConfigurationSection("players." + player.getUniqueId() + ".homes.home") != null
+                    && Bukkit.getWorld(UUID.fromString(FileManager.getInstance().storage.getString("" + player.getUniqueId() + ".homes.home.world"))) != null) {
+                teleportPlayer.setHome(new Location(
+                        Bukkit.getWorld(UUID.fromString(FileManager.getInstance().storage.getString("" + player.getUniqueId() + ".homes.home.world"))),
+                        FileManager.getInstance().storage.getDouble("players." + player.getUniqueId() + ".homes.home.x"),
+                        FileManager.getInstance().storage.getDouble("players." + player.getUniqueId() + ".homes.home.y"),
+                        FileManager.getInstance().storage.getDouble("players." + player.getUniqueId() + ".homes.home.z"),
+                        Float.parseFloat(FileManager.getInstance().storage.getString("players." + player.getUniqueId() + ".homes.home.yaw")),
+                        Float.parseFloat(FileManager.getInstance().storage.getString("players." + player.getUniqueId() + ".homes.home.pitch"))
+                ));
+            }
+
+        });
 
     }
 
+    void runDecrement() {
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+
+            TeleportPlayer teleportPlayer = TeleportPlayer.getTeleportPlayer(player);
+
+            if (teleportPlayer.getCooldownMultiplier() > 1) {
+                teleportPlayer.setCooldownMultiplier(teleportPlayer.getCooldownMultiplier() - getConfig().getDouble("periodic.cooldown-multiplier"));
+                if (teleportPlayer.getCooldownMultiplier() < 1)
+                    teleportPlayer.setCooldownMultiplier(1);
+            }
+
+            if (teleportPlayer.getCostMultiplier() > 1) {
+                teleportPlayer.setCostMultiplier(teleportPlayer.getCooldownMultiplier() - getConfig().getDouble("periodic.cost-multiplier"));
+                if (teleportPlayer.getCostMultiplier() < 1)
+                    teleportPlayer.setCostMultiplier(1);
+            }
+
+            if (ConfigHelper.debug) {
+                getLogger().info("Decrementing multipliers");
+                getLogger().info("New cooldown multiplier for " + player.getName() + ": " + teleportPlayer.getCooldownMultiplier());
+                getLogger().info("New cost multiplier for " + player.getName() + ": " + teleportPlayer.getCostMultiplier());
+            }
+
+        }), 0L, getConfig().getInt("periodic.frequency"));
+    }
 }
