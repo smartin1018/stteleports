1dc2e18e3a3e3d088c9a5389c386f7c8df5afdf0
Jerred Shepherd <shepherdjerred@gmail.com>
2017-01-19 12:12:57 -0600

    Make limit final

--- src/main/java/com/shepherdjerred/stteleports/util/TeleportQueue.java
+++ src/main/java/com/shepherdjerred/stteleports/util/TeleportQueue.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class TeleportQueue<E> extends LinkedList<E> {
 
-    private int limit;
+    private final int limit;
 
     public TeleportQueue(int limit) {
         this.limit = limit;
